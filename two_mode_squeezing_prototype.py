d80adeced98c7c079d8ee89895cd2bea8f8e2b95
Lukas Tarra <tarra@acin.tuwien.ac.at>
2026-01-15 16:16:18 +0100

    implemented the optimal feedforward to find J(t) such that number squeezing is optimized at the final time
    next up: match this with A(t) such that J(t) is matched.

--- two_mode_squeezing_prototype.py
+++ two_mode_squeezing_prototype.py
@@ -3,13 +3,14 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 import jax.numpy as jnp
-from jax import jit, vmap, random # , profiler
+from jax import jit, vmap, random, jacobian # , profiler
 import jax.debug as jdebug
-from jax.lax import scan
+from jax.lax import scan, fori_loop
 from functools import partial
 import pdb
 # import jax.scipy.optimize as jopt
 from jaxopt import ScipyBoundedMinimize
+from jaxopt import AndersonAcceleration
 # broader shell output before linebreaks for debugging 
 np.set_printoptions(linewidth=300, edgeitems=10)
 
@@ -232,17 +233,16 @@ sim_forward_vmap = (vmap(simulate_single_traj, in_axes=(None, None, 0, None, Non
 @jit
 def traj_cost_function(input_array, traj, t_array, cost_weights, input_base, f_p):
     c_n, c_J_deriv, c_J_abs = cost_weights
-    T = t_array[-1]
+    # T = t_array[-1]
     delta_t = 1/(4*jnp.pi*f_p)
     var_n_traj = jnp.var(traj[:, :, 0], axis=0)
-    # n_traj = traj[:, :, 0]
-    # cost_n = c_n/T * jnp.mean(jnp.trapezoid(n_traj**2, x=t_array, axis=1) , axis=0)
-    cost_n = c_n/T * jnp.trapezoid(var_n_traj, x=t_array)
-    # cost_n = c_n * jnp.min(var_n_traj)
-    # cost_J_deriv = c_J_deriv/T * (delta_t/input_base)**2 * jnp.trapezoid(jnp.gradient(input_array,t_array)**2, x=t_array)
-    # cost_J_abs = c_J_abs * jnp.mean(input_array)/input_base
+    # cost_n = c_n / T * jnp.trapezoid(var_n_traj, x=t_array)
+    cost_n = c_n * jnp.trapezoid(var_n_traj, x=t_array)
+    cost_J_deriv = c_J_deriv * (delta_t/input_base)**2 * jnp.trapezoid(jnp.gradient(input_array,t_array)**2, x=t_array)
+    # cost_J_abs = 0
+    cost_J_abs = c_J_abs * jnp.trapezoid(((input_array-input_base)/input_base)**2, x=t_array)
     
-    return cost_n
+    return cost_n + cost_J_deriv + cost_J_abs
 
 @jit
 def input_guess_fun(t, input_amplitude, input_frequency, input_phase, input_offset, base_parameters):
@@ -310,8 +310,10 @@ def optimize_J_guess_fun_BFGS(initial_opt_var, base_parameters, cost_weights, J_
     
 #     return lbfgsb_sol.params
 
+
+# might cut n_avg from the input as this becomes more settled
 @jit
-def costate_step(x, J, U, dt, params):
+def costate_step(x, J, U, n, n_avg, Phi, num_traj, c_n, dt, params):
     """
     Perform one RK4 step for the costates of the optimality conditions.
     
@@ -322,13 +324,16 @@ def costate_step(x, J, U, dt, params):
     # Load parameters
     hbar = params['hbar']
     N = params['N']
-    Delta_E = params['E']
+    # pre-compute common expressions 
     two_J_by_hbar = (2*J/hbar)
+    sqrt_1_minus_n2 = jnp.sqrt(1-n**2)
 
     # define rhs of the optimality condition ODE
     def rhs(x):
         lambda_n, lambda_Phi = x
-        dlambda_n_dt = -c_n/(num_traj*T) *2*(1-1/num_traj)*(n-n_avg) - 2*J/hbar*n/jnp.sqrt(1-n**2)*jnp.sin(Phi)*lambda_n - 1/hbar*(N*U+2*J/((1-n**2)**(3/2))*jnp.cos(Phi))*lambda_Phi
+        # dlambda_n_dt = -c_n/(num_traj) *2*(n-n_avg) - two_J_by_hbar *n/sqrt_1_minus_n2*jnp.sin(Phi)*lambda_n - 1/hbar*(N*U+2*J/((1-n**2)**(3/2))*jnp.cos(Phi))*lambda_Phi
+        dlambda_n_dt = - two_J_by_hbar *n/sqrt_1_minus_n2*jnp.sin(Phi)*lambda_n - 1/hbar*(N*U+2*J/((1-n**2)**(3/2))*jnp.cos(Phi))*lambda_Phi
+        dlambda_Phi_dt = two_J_by_hbar* ( sqrt_1_minus_n2*jnp.cos(Phi)*lambda_n + n/sqrt_1_minus_n2*jnp.sin(Phi)*lambda_Phi )
     
         return jnp.array([dlambda_n_dt, dlambda_Phi_dt])
 
@@ -345,24 +350,102 @@ def costate_step(x, J, U, dt, params):
     k4 = dt * rhs(x + k3)
     
     return x + (k1 + 2*k2 + 2*k3 + k4) / 6
-    
+
 # @jit
-def simulate_costate_traj(J_traj, U_traj, x_0, dt, params):
+def simulate_costate_traj(J_traj, U_traj, n_traj, n_avg_traj, Phi_traj, x_0, num_traj, c_n, dt, params):
     """Simulate one trajectory of the Josephson system for one initial state."""
     
     def step_fn(carry, inputs):
         x = carry
-        J, U = inputs
+        J, U, n, n_avg, Phi = inputs
         
-        x_next = costate_step(x, J, U, dt, params)
+        x_next = costate_step(x, J, U, n, n_avg, Phi, num_traj, c_n, dt, params)
         return x_next, x_next
 
-    inputs = (J_traj, U_traj)
+    inputs = (J_traj, U_traj, n_traj, n_avg_traj, Phi_traj)
     _, traj = scan(step_fn, x_0, inputs)
     
     return traj
 
-sim_costate_vmap = (vmap(simulate_costate_traj, in_axes=(None, None, 0, None, None), out_axes=0))
+sim_costate_vmap = (vmap(simulate_costate_traj, in_axes=(None, None, 0, None, 0, 0, None, None, None, None), out_axes=0))
+
+
+@jit
+def J_array_iteration(J_in_array, U_array, x_0_array, dt, t_array, c_n, c_J, J_base, params):
+    # load parameters
+    hbar = params['hbar']
+    num_traj = x_0_array.shape[0]
+    
+    # simulate the Josephson system foward for n and Phi
+    traj = sim_forward_vmap(J_in_array, U_array, x_0_array, dt, params)
+    n_traj = traj[:,:,0]
+    Phi_traj = traj[:,:,1]
+    n_avg_traj = jnp.mean(n_traj, axis=0)
+    # lambda_0_array = jnp.zeros_like(x_0_array) # the costate at final time is zero due to the cost function's definitions
+    lambda_n_0_array = 2*c_n/num_traj* ( n_traj[:,-1]-n_avg_traj[-1] )
+    lambda_Phi_0_array = jnp.zeros_like(lambda_n_0_array)
+    # lambda_0_array = jnp.concatenate((lambda_n_0_array, lambda_Phi_0_array), axis=1)
+    lambda_0_array = jnp.column_stack([lambda_n_0_array, lambda_Phi_0_array])
+    
+    # simulate the costate system backward, starting from lambda_0_array
+    lambda_traj_rev = sim_costate_vmap(J_in_array[::-1], U_array[::-1], n_traj[:, ::-1], n_avg_traj[::-1], Phi_traj[:, ::-1], lambda_0_array, num_traj, c_n, -1* dt, params)
+
+    lambda_traj = lambda_traj_rev[:, ::-1, :]
+    lambda_n_traj = lambda_traj[:,:,0]
+    lambda_Phi_traj = lambda_traj[:,:,1]
+    
+    # Perform control update for J
+    sqrt_1_minus_n2 = jnp.sqrt(1-n_traj**2)
+    J_out_array = J_base + J_base**2/(hbar*c_J)* jnp.sum( sqrt_1_minus_n2*jnp.sin(Phi_traj)*lambda_n_traj - n_traj/sqrt_1_minus_n2*jnp.cos(Phi_traj)*lambda_Phi_traj , axis=0 )
+    # J_out_array = ( J_base + J_base**2/(hbar*c_J)* jnp.sum( sqrt_1_minus_n2*jnp.sin(Phi_traj)*lambda_n_traj - n_traj/sqrt_1_minus_n2*jnp.cos(Phi_traj)*lambda_Phi_traj , axis=0 ) ) * jnp.exp(0.5*t_array/t_array[-1])
+    
+    return J_out_array
+
+def run_simple_J_descent(J_init_array, U_array, x_0_array, dt, t_array, c_n, c_J, J_base, params, max_iter=50):
+    """Iterate the J_array iteration for max_iter iterations."""
+
+    def iteration_wrapper(i, val):
+        return J_array_iteration(val, U_array, x_0_array, dt, t_array, c_n, c_J, J_base, params)
+    
+    J_out_array = fori_loop(0, max_iter, iteration_wrapper, J_init_array)
+
+    return J_out_array
+
+
+def run_Newton_J_descent(J_init_array, U_array, x_0_array, dt, t_array, c_n, c_J, J_base, params, max_iter=50):
+    """Iterate the J_array iteration for max_iter iterations."""
+
+    @jit
+    def f(val):
+        return J_array_iteration(val, U_array, x_0_array, dt, t_array, c_n, c_J, J_base, params) - val
+    
+    f_Jacobian = jacobian(f)
+
+    def Newton_step(i, val):
+
+        # tmp = jnp.linalg.solve( f_Jacobian(val), f(val) )
+        # jdabug.breakpoint()
+        
+        return val - jnp.linalg.solve( f_Jacobian(val), f(val) )
+
+    J_out_array = fori_loop(0, max_iter, Newton_step, J_init_array)
+    
+    return J_out_array
+
+
+def run_FixedPoint_J_descent(J_init_array, U_array, x_0_array, dt, t_array, c_n, c_J, J_base, params):
+    """Iterate the J_array iteration for max_iter iterations."""
+
+    def f(val, U_array, x_0_array, dt, t_array, c_n, c_J, J_base, params):
+        return J_array_iteration(val, U_array, x_0_array, dt, t_array, c_n, c_J, J_base, params)
+    
+    # Configure history_size (m) based on your problem
+    # Higher m = faster convergence but more memory and potential instability
+    accel = AndersonAcceleration(f)
+    J_out_array = accel.run(J_init_array, U_array, x_0_array, dt, t_array, c_n, c_J, J_base, params)  # initial_p is your initial guess
+    
+    return J_out_array.params
+
 
 
 if __name__ == "__main__":
@@ -387,17 +470,18 @@ if __name__ == "__main__":
     variance_0_Phi =   1*  sqrt_1_plus_Lambda / params['N']
 
     # Generate num_trajectories random initial states (n, Phi) with Gaussian distribution
-    rng = random.PRNGKey(1)  # For reproducibility
-    num_trajectories = 500
+    num_trajectories = 550
     # Sample from Gaussian distributions for n and Phi
-    n_samples = random.normal(rng, (num_trajectories,)) * np.sqrt(variance_0_n)
-    Phi_samples = random.normal(rng, (num_trajectories,)) * np.sqrt(variance_0_Phi)
+    rng_n = random.PRNGKey(10)  # For reproducibility
+    n_samples = random.normal(rng_n, (num_trajectories,)) * np.sqrt(variance_0_n)
+    rng_Phi = random.PRNGKey(42)
+    Phi_samples = random.normal(rng_Phi, (num_trajectories,)) * np.sqrt(variance_0_Phi)
     
     # Simulation settings
     Phi_0_mean = jnp.mean(Phi_samples)
     plasma_frequency = 2*J_baseline/(params['hbar']*2*jnp.pi) * jnp.sqrt(Phi_0_mean + Lambda_baseline)
     # t_final = 3/plasma_frequency
-    t_final = 50e-3
+    t_final = 40e-3
     num_steps = int(500)
     dt = t_final / num_steps
     time_array = jnp.linspace(0, t_final, num_steps)
@@ -412,13 +496,26 @@ if __name__ == "__main__":
     J_traj = jnp.ones(num_steps) * J_baseline
     U_traj = jnp.ones(num_steps) * U_baseline
 
-    # optimize the trajectory for J
-    initial_opt_var = jnp.array([0.1, 2, 0.5, 1])
-    opt_base_parameters = J_baseline, plasma_frequency, jnp.pi, J_baseline
-    # initial_opt_var = jnp.array([0.2*U_baseline, 1.25*jnp.pi/2, 0.8*U_baseline])
-    c_params = 1000, 0, 0
-    opt_variables_out = np.array(optimize_J_guess_fun_BFGS(initial_opt_var, opt_base_parameters, c_params, J_baseline, plasma_frequency, U_traj, x_0_array, time_array, dt, params))
-    J_traj = input_guess_fun(time_array, opt_variables_out[0], opt_variables_out[1], opt_variables_out[2], opt_variables_out[3], opt_base_parameters)
+    # # optimize the trajectory (basis function approach) for J
+    # initial_opt_var = jnp.array([0.1, 2, 0.5, 1])
+    # opt_base_parameters = J_baseline, plasma_frequency, jnp.pi, J_baseline
+    # # initial_opt_var = jnp.array([0.2*U_baseline, 1.25*jnp.pi/2, 0.8*U_baseline])
+    # c_params = 1e7, 1e2, 1e2
+    # opt_variables_out = np.array(optimize_J_guess_fun_BFGS(initial_opt_var, opt_base_parameters, c_params, J_baseline, plasma_frequency, U_traj, x_0_array, time_array, dt, params))
+    # J_traj = input_guess_fun(time_array, opt_variables_out[0], opt_variables_out[1], opt_variables_out[2], opt_variables_out[3], opt_base_parameters)
+    # print("Initial relative parameters:")
+    # print(np.array(initial_opt_var))    
+    # print("Optimized relative parameters:")
+    # print(opt_variables_out)
+    # #print("Resulting cost value:")
+    # #print(np.array(traj_cost_function(J_traj, trajectories, time_array, c_params, opt_base_parameters, plasma_frequency)))
+
+    # J_traj = run_simple_J_descent(J_traj, U_traj, x_0_array, dt, 100, 0.3, J_baseline, params, 100)
+    # J_traj = run_Newton_J_descent(J_traj, U_traj, x_0_array, dt, time_array, 300, 0.08, J_baseline, params, 4)
+    J_traj = run_FixedPoint_J_descent(J_traj, U_traj, x_0_array, dt, time_array, 5e0, 10e-2, J_baseline, params)
+    J_traj = run_Newton_J_descent(J_traj, U_traj, x_0_array, dt, time_array, 2e1, 1e-2, J_baseline, params, 2)
+
+    # J_traj = J_baseline* ( jnp.ones_like(J_traj) + jnp.sin(4*jnp.pi*plasma_frequency * time_array) )    
     
     # run simulation with the optimized trajectory
     # Time the simulation
@@ -430,20 +527,11 @@ if __name__ == "__main__":
     timer.stop()
     print(f"Simulation completed in {timer.elapsed():.4f} seconds")
 
-    print("Initial relative parameters:")
-    print(np.array(initial_opt_var))    
-    print("Optimized relative parameters:")
-    print(opt_variables_out)
-    print("Resulting cost value:")
-    print(np.array(traj_cost_function(J_traj, trajectories, time_array, c_params, opt_base_parameters, plasma_frequency)))
-
     
     # create SimResults object
     sim_results = SimResults(time_array, trajectories, (variance_0_n, variance_0_Phi), J_traj )
     # plot trajectories and variances
     sim_results.plot_trajectories(N=num_trajectories, plotting_sample_step=1, plot_mean=True)
     sim_results.plot_variances(plotting_sample_step=1)
-
-
 
     
